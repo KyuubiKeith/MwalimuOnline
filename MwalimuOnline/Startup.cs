6d46cc545e1f1e0689084184e2c55a6d1971888f
Keith Otinya <otinya@miyabiafrica.com>
2018-08-14 19:03:01 +0300

    We Begin

---
+++ MwalimuOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MwalimuOnline.Startup))]
+namespace MwalimuOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
